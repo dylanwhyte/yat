5c4b4034e10c442330d85124ee4844acf848430b
Dylan Whyte <dylantwhyte@proton.me>
2022-08-26 21:48:08 +0200

    Add cpal_config struct
    Used to have common access to cpal configuration for items such as
    sample rate

---
+++ src/cpal_config.rs
@@ -0,0 +1,22 @@
+use cpal::{self, Host, Device, SupportedStreamConfig};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
+pub struct CpalConfig {
+    pub host: Host,
+    pub device: Device,
+    pub config: SupportedStreamConfig,
+}
+
+impl CpalConfig {
+    pub fn new() -> Self {
+        let host = cpal::default_host();
+        let device = host.default_output_device().expect("no device available");
+        let config = device.default_output_config().unwrap();
+
+        Self {
+            host,
+            device,
+            config,
+        }
+    }
+}
